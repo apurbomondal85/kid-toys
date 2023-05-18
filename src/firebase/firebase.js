bc9eba633e37e0125308a3f4c35b763f8350b094
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2023-05-18 08:33:10 +0600

    all file set up is done for education toys project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxqdjltv8DmQcf4sGTYw1ALQdC6FljpSk",
+  authDomain: "education-toys.firebaseapp.com",
+  projectId: "education-toys",
+  storageBucket: "education-toys.appspot.com",
+  messagingSenderId: "864636575404",
+  appId: "1:864636575404:web:cce861a64157f9a511c909"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
